f72eea434218943b1124a61734c00b4687f2481d
nada <nadaelneel29@gmail.com>
2023-12-30 17:26:27 +0200

    second commit

--- Models/MVC/wwwroot/js/getStock.js
+++ Models/MVC/wwwroot/js/getStock.js
@@ -1,30 +1,21 @@
-var firstValue = $('#selectedStore').val();
-var secondValue = $('#selectedItem').val();
-$.ajax({
-    url: '/Store_Item/GetStock',
-    type: 'GET',
-    data: { store_id: firstValue, item_id: secondValue },
-    success: function (result) {
-        $('#stock').val(result);
-    },
-    error: function () {
-        alert('Error occurred while retrieving data.');
-    }
-});
+myfun();
     $(document).ready(function () {
         $('#selectedStore , #selectedItem').change(function () {
-            var firstValue = $('#selectedStore').val();
-            var secondValue = $('#selectedItem').val();
-            $.ajax({
-                url: '/Store_Item/GetStock',
-                type: 'GET',
-                data: { store_id: firstValue, item_id: secondValue },
-                success: function (result) {
-                    $('#stock').val(result);
-                },
-                error: function () {
-                    alert('Error occurred while retrieving data.');
-                }
-            });
+            myfun();
         });
     });
+function myfun() {
+    var firstValue = $('#selectedStore').val();
+    var secondValue = $('#selectedItem').val();
+    $.ajax({
+        url: '/Store_Item/GetStock',
+        type: 'GET',
+        data: { store_id: firstValue, item_id: secondValue },
+        success: function (result) {
+            $('#stock').val(result);
+        },
+        error: function () {
+            alert('Error occurred while retrieving data.');
+        }
+    });
+}
